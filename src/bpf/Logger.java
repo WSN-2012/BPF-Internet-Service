c3e06ae95bc5dabb1968e6f11f78a75a0a2ca994
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-10-24 11:58:57 +0200

    Logger class implemented

--- src/bpf/Logger.java
+++ src/bpf/Logger.java
@@ -4,22 +4,25 @@ import se.kth.ssvl.tslab.wsn.general.bpf.BPFLogger;
 
 public class Logger implements BPFLogger {
 
-	public String debug(String arg0, String arg1) {
-		return null;
+	private String log(String text) {
+		System.out.println(text);
+		return text;
 	}
-
-	public String error(String arg0, String arg1) {
-		return null;
-	}
-
-	public String info(String arg0, String arg1) {
-		return null;
-	}
-
-	public String warning(String arg0, String arg1) {
-		return null;
-	}
-
 	
+	public String debug(String TAG, String text) {
+		return log(String.format("[DEBUG] {0}:{1}", TAG, text));
+	}
+
+	public String error(String TAG, String text) {
+		return log(String.format("[ERROR] {0}:{1}", TAG, text));
+	}
+
+	public String info(String TAG, String text) {
+		return log(String.format("[INFO] {0}:{1}", TAG, text));
+	}
+
+	public String warning(String TAG, String text) {
+		return log(String.format("[WARN] {0}:{1}", TAG, text));
+	}
 	
 }
