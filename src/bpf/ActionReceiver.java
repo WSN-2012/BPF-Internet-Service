35ccd5c9f0eb055e7c6c24790ecf91fcf745fc9d
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-10-24 11:59:35 +0200

    First draft of the action receiver is implemented

--- src/bpf/ActionReceiver.java
+++ src/bpf/ActionReceiver.java
@@ -2,13 +2,30 @@ package bpf;
 
 import se.kth.ssvl.tslab.wsn.general.bpf.BPFActionReceiver;
 import se.kth.ssvl.tslab.wsn.general.servlib.bundling.bundles.Bundle;
+import se.kth.ssvl.tslab.wsn.general.servlib.bundling.exception.BundleLockNotHeldByCurrentThread;
 
 public class ActionReceiver implements BPFActionReceiver {
 
-	public void bundleReceived(Bundle arg0) {
+	private static final String TAG = "Actions";
+	
+	private Logger logger;
+	
+	
+	public ActionReceiver(Logger _logger) {
+		logger = _logger;
+	}
+	
+	public void bundleReceived(Bundle bundle) {
+		logger.debug(TAG, "Received bundle! Reading:");
+		try {
+			logger.debug(TAG, new String(bundle.payload().memory_buf()));
+		} catch (BundleLockNotHeldByCurrentThread e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void notify(String arg0, String arg1) {
+	public void notify(String header, String description) {
+		logger.debug(TAG, String.format("[{0}] {1}", header, description));
 	}
 
 	
