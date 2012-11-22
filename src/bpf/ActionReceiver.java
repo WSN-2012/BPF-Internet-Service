3e5e54ca3e7a976121d6de428eea7ca9770dfbf3
Fabio Viggiani <viggiani.fabio@gmail.com>
2012-11-22 14:42:18 +0100

    Implemented method updateStats in ActionReceiver. Updated BPF.

--- src/bpf/ActionReceiver.java
+++ src/bpf/ActionReceiver.java
@@ -8,6 +8,7 @@ import se.kth.ssvl.tslab.wsn.general.bpf.BPF;
 import se.kth.ssvl.tslab.wsn.general.bpf.BPFActionReceiver;
 import se.kth.ssvl.tslab.wsn.general.servlib.bundling.bundles.Bundle;
 import se.kth.ssvl.tslab.wsn.general.servlib.bundling.exception.BundleLockNotHeldByCurrentThread;
+import se.kth.ssvl.tslab.wsn.general.servlib.storage.Stats;
 
 public class ActionReceiver implements BPFActionReceiver {
 
@@ -60,4 +61,11 @@ public class ActionReceiver implements BPFActionReceiver {
 		logger.info(TAG, header + ":" + description);
 	}
 
+	public void updateStats (Stats stats) {
+		logger.debug(TAG, "New Stats object received:" +
+				"\nTotal size: " + stats.totalSize() + 
+				"\nStored bundles: " + stats.storedBundles() +
+				"\nTransmitted bundles: " + stats.transmittedBundles() +
+				"\nReceived bundles: " + stats.receivedBundles() );
+	}
 }
