0d830f551a28d8f20c221677f89cd2c22b258130
Fabio Viggiani <viggiani.fabio@gmail.com>
2012-12-03 12:18:28 +0100

    Getting broadcast address in Communication

--- src/bpf/Communication.java
+++ src/bpf/Communication.java
@@ -1,19 +1,46 @@
 package bpf;
 
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
+import se.kth.ssvl.tslab.wsn.general.bpf.BPF;
 import se.kth.ssvl.tslab.wsn.general.bpf.BPFCommunication;
 
 public class Communication implements BPFCommunication {
 
+	private static final String TAG = "Communication";
+	
 	public InetAddress getBroadcastAddress() {
+		InetAddress foundBcastAddress = null;
+		System.setProperty("java.net.preferIPv4Stack", "true");
 		try {
-			return InetAddress.getByAddress(new byte[] {(byte) 255,(byte) 255,(byte) 255,(byte) 255});
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			Enumeration<NetworkInterface> niEnum = NetworkInterface
+					.getNetworkInterfaces();
+			while (niEnum.hasMoreElements()) {
+				NetworkInterface ni = niEnum.nextElement();
+				if (!ni.isLoopback()) {
+					for (InterfaceAddress interfaceAddress : ni
+							.getInterfaceAddresses()) {
+						foundBcastAddress = interfaceAddress.getBroadcast();
+					}
+				}
+			}
+		} catch (SocketException e) {
+			BPF.getInstance().getBPFLogger()
+					.error(TAG, "Exception while getting broadcast address.");
 		}
-		return null;
+
+		if (foundBcastAddress != null) {
+			BPF.getInstance().getBPFLogger().debug(TAG, "Called getBroadcastAddress. Returning: " + foundBcastAddress.getHostAddress());
+		} else {
+			BPF.getInstance().getBPFLogger().warning(TAG, "Called getBroadcastAddress but foundBcastAddress is null!");
+		}
+		
+		return foundBcastAddress;
 	}
 
 	public InetAddress getDeviceIP() {
@@ -24,5 +51,5 @@ public class Communication implements BPFCommunication {
 		}
 		return null;
 	}
-
+	
 }
